5f9c9d390408284275398d717c82034c9e32ea3c
jvverde at win10 <jvverde@win10>
2020-11-09 17:24:36 +0000

    Readme

--- README.md
+++ README.md
@@ -1,2 +1,211 @@
-# fulldeepequal
-Full deep objects
+# isClone
+Deep compare two objects/values and return true if one is a clone of other
+
+## Setup
+
+**1. Install**
+
+```shell
+npm install isclone
+```
+
+## Usage
+
+```javascript
+//Primitive types
+isClone(null, undefined)  // FALSE
+isClone(undefined, undefined)  // TRUE
+isClone(null, null)  // TRUE
+isClone(3/0, 3/0)  // TRUE
+isClone(+"xpto", +"1 + 2")  // TRUE
+isClone(3, 4)  // FALSE
+isClone(3, 3)  // TRUE
+isClone('abc', 'abc')  // TRUE
+isClone('abc', '')  // FALSE
+
+//Primitive types and builtin objects
+isClone(3, new Number(3))  // FALSE
+isClone(BigInt(321), 321n)  // TRUE
+isClone(3, '3')  // FALSE
+isClone(3, new Number(3))  // FALSE
+isClone(new String('ab'), new String('ab'))  // TRUE
+isClone(new String('ab'), 'ab')  // FALSE
+isClone(new Boolean(true), new Boolean(true))  // TRUE
+isClone(true, true)  // TRUE
+const now = new Date
+isClone(now, new Date(now))  // TRUE
+isClone(now, new Date('December 17, 1995 03:24:00'))  // FALSE
+const re = /^$/imsg
+isClone(re, new RegExp(re))  // TRUE
+isClone(/^$/igsm, /^$/sgmi)  // TRUE
+isClone(/^$/igsm, /^$/im)  // FALSE
+isClone(/^.$/, /^$/)  // FALSE
+
+//Arays and binary arrays
+isClone([3, 2], [3, 2])  // TRUE
+isClone([3, 2], [3, 2, 1])  // FALSE
+isClone(new Int8Array([3, -3]), new Int8Array([3, -3]))  // TRUE
+isClone(new Int8Array([3, -3]), new Int8Array([3, -3, 4]))  // FALSE
+isClone(new BigInt64Array([3n, -3n]), new BigInt64Array([3n, -3n]))  // TRUE
+isClone(new BigInt64Array([3n, -3n]), new BigInt64Array([3n, 0n]))  // FALSE
+isClone(new Float64Array([Math.PI, Math.E, 1/3, Math.sqrt(2)]), new Float64Array([Math.PI, Math.E, 1/3, Math.sqrt(2)]))  // TRUE
+
+// Functions (by default two function are equal if and only if they both refere to same code/object)
+const f = function() {}
+const g = function() {}
+isClone(f, g)  // FALSE
+isClone(f, g, {strictly: false})  // TRUE
+
+//Literal Objects
+const x = { i: 1 }
+const y = { i: 1 }
+isClone(x, y)  // TRUE
+
+//Nest Objects
+const z = { a: x }
+const w = { a: y }
+isClone(z, w)  // TRUE
+
+//Object members
+w.f = () => {}
+isClone(z, w)  // FALSE
+
+//User defined Objects
+function A (i) { this.i = i }
+function B (i) { this.i = i }
+isClone(A, B)  // FALSE
+const a = new A(3)
+isClone(a, new A(3))  // TRUE
+isClone(a, new A(4))  // FALSE
+const b = new B(3)
+isClone(a, b)  // FALSE
+
+//Sets
+const s = new Set([x, a])
+isClone(s, new Set(s))  // TRUE
+isClone(s, new Set([x, a]))  // TRUE
+isClone(s, new Set([x, b]))  // FALSE
+
+//Maps
+const m = new Map([[x, a], [y, a]])
+isClone(m, new Map([[x, a], [y, b]]))  // FALSE
+
+// Class instances
+class C{}
+isClone(C, A)  // FALSE
+const c = new C()
+isClone(c, new C())  // TRUE
+
+// Inheritance
+class D extends C {}
+isClone(D, C)  // FALSE
+const d = new D()
+isClone(d, c)  // FALSE
+isClone(d, new D())  // TRUE
+
+// Changing an instance property
+class E{
+  constructor (i) { this.i = i }
+  inc() { this.i++ }
+}
+const e = new E(0)
+isClone(e, new E(0))  // TRUE
+e.inc = function(){}
+isClone(e, new E(0))  // FALSE
+
+// Nested objects
+class DE{
+  constructor (i) { this.e = new E(i) }
+}
+const de = new DE(0)
+isClone(de, new DE(0))  // TRUE
+isClone(new DE(c), new DE(c))  // TRUE
+isClone(new DE(de), new DE(de))  // TRUE
+isClone(new DE(m), new DE(m))  // TRUE
+de.x = 'x'
+isClone(de, new DE(0))  // FALSE
+
+// Circular references
+x.i = x
+y.i = x
+isClone(x, y)  // TRUE
+x.i = x
+y.i = y
+isClone(x, y)  // TRUE
+x.i = y
+y.i = x
+isClone(x, y)  // TRUE
+
+// Subclasses of builtin types
+const abc = new String('abc')
+class STR extends String{
+  constructor(s) {super(s)}
+}
+isClone(new STR('abc'), new STR('abc'))  // TRUE
+isClone(new STR('abc'), abc)  // FALSE
+
+abc.label = 'label'
+isClone(abc, new String('abc'))  // FALSE
+
+const one = new Number(1)
+one.label = 'label'
+isClone(one, new Number(1))  // FALSE
+
+const bool = new Boolean(true)
+bool.label = 'label'
+isClone(bool, new Boolean(true))  // FALSE
+
+const d1st = new Date('December 1, 1995 00:00:00')
+isClone(d1st, new Date('December 1, 1995 00:00:00'))  // TRUE
+d1st.label = 'label'
+isClone(d1st, new Date('December 1, 1995 00:00:00'))  // FALSE
+
+const regexp = /^.+$/imsg
+isClone(regexp, /^.+$/imsg)  // TRUE
+regexp.label = 'label'
+isClone(regexp, /^.+$/imsg)  // FALSE
+
+
+const array = [1, 2]
+isClone(array, [1, 2])  // TRUE
+array.label = 'label'
+isClone(array, [1, 2])  // FALSE
+
+class Vector extends Array{
+  constructor(s) {super(s)}
+}
+isClone(new Vector(3), new Vector(3))  // TRUE
+isClone(new Vector(3), new Array(3))  // FALSE
+
+const set = new Set([1, 2])
+isClone(set, new Set([1, 2]))  // TRUE
+set.label = 'label'
+isClone(set, new Set([1, 2]))  // FALSE
+
+const map = new Map([[1, 2], [2,3]])
+isClone(map, new Map([[1, 2], [2,3]]))  // TRUE
+map.label = 'label'
+isClone(map, new Map([[1, 2], [2,3]]))  // FALSE
+
+// Prototype inheritance
+function A(n = 1) { this.n = n }
+A.prototype.inc = function(n) { this.n += n }
+const obja = new A(1)
+isClone(obja, new A(1))  // TRUE
+isClone(obja, new A(2))  // FALSE
+
+function B(n = 1) { A.call(this, n) }
+isClone(obja, new B(1))  // FALSE
+B.prototype = Object.create(A.prototype)
+isClone(obja, new B(1))  // FALSE
+B.prototype.dec = function(n) { this.inc(-n); }
+B.prototype.constructor = B
+
+isClone(A, B)  // FALSE
+const objb = new B(1)
+isClone(objb, new B(1))  // TRUE
+isClone(objb, obja)  // FALSE
+objb.inc = () => {}
+isClone(objb, new B(1))  // FALSE
+```
+
--- examples/examples.js
+++ examples/examples.js
@@ -1,3 +1,5 @@
+// https://developer.mozilla.org/en-US/docs/Learn/JavaScript/Objects/Inheritance
+
 const isClone = require('../isClone')
 const yargs = require('yargs');
 
@@ -24,12 +26,18 @@ const assert = {
   ne: (x, y, a) => console.log(cnt++, isClone(x, y, { ...a, debug }) ? 'FAIL' : 'OK', show(x, 'ne', y))
 }
 
+//Primitive types
 assert.ne(null, undefined)
 assert.eq(undefined, undefined)
 assert.eq(null, null)
 assert.eq(3/0, 3/0)
 assert.eq(+"xpto", +"1 + 2")
 assert.ne(3, 4)
+assert.eq(3, 3)
+assert.eq('abc', 'abc')
+assert.ne('abc', '')
+
+//Primitive types and builtin objects
 assert.ne(3, new Number(3))
 assert.eq(BigInt(321), 321n)
 assert.ne(3, '3')
@@ -38,15 +46,16 @@ assert.eq(new String('ab'), new String('ab'))
 assert.ne(new String('ab'), 'ab')
 assert.eq(new Boolean(true), new Boolean(true))
 assert.eq(true, true)
-const datea = new Date
-const dateb = new Date(datea)
-assert.eq(datea, dateb)
-assert.ne(datea, new Date('December 17, 1995 03:24:00'))
+const now = new Date
+assert.eq(now, new Date(now))
+assert.ne(now, new Date('December 17, 1995 03:24:00'))
 const re = /^$/imsg
 assert.eq(re, new RegExp(re))
-assert.eq(/^$/igsm, /^$/imsg)
-assert.ne(/^$/igsm, /^$/ims)
+assert.eq(/^$/igsm, /^$/sgmi)
+assert.ne(/^$/igsm, /^$/im)
 assert.ne(/^.$/, /^$/)
+
+//Arays and binary arrays
 assert.eq([3, 2], [3, 2])
 assert.ne([3, 2], [3, 2, 1])
 assert.eq(new Int8Array([3, -3]), new Int8Array([3, -3]))
@@ -54,13 +63,28 @@ assert.ne(new Int8Array([3, -3]), new Int8Array([3, -3, 4]))
 assert.eq(new BigInt64Array([3n, -3n]), new BigInt64Array([3n, -3n]))
 assert.ne(new BigInt64Array([3n, -3n]), new BigInt64Array([3n, 0n]))
 assert.eq(new Float64Array([Math.PI, Math.E, 1/3, Math.sqrt(2)]), new Float64Array([Math.PI, Math.E, 1/3, Math.sqrt(2)]))
+
+// Functions (by default two function are equal if and only if they both refere to same code/object)
 const f = function() {}
 const g = function() {}
 assert.ne(f, g)
 assert.eq(f, g, {strictly: false})
+
+//Literal Objects
 const x = { i: 1 }
 const y = { i: 1 }
 assert.eq(x, y)
+
+//Nest Objects
+const z = { a: x }
+const w = { a: y }
+assert.eq(z, w)
+
+//Object members
+w.f = () => {}
+assert.ne(z, w)
+
+//User defined Objects
 function A (i) { this.i = i }
 function B (i) { this.i = i }
 assert.ne(A, B)
@@ -69,23 +93,31 @@ assert.eq(a, new A(3))
 assert.ne(a, new A(4))
 const b = new B(3)
 assert.ne(a, b)
+
+//Sets
 const s = new Set([x, a])
 assert.eq(s, new Set(s))
 assert.eq(s, new Set([x, a]))
 assert.ne(s, new Set([x, b]))
+
+//Maps
 const m = new Map([[x, a], [y, a]])
 assert.ne(m, new Map([[x, a], [y, b]]))
 
+// Class instances
 class C{}
 assert.ne(C, A)
 const c = new C()
 assert.eq(c, new C())
+
+// Inheritance
 class D extends C {}
 assert.ne(D, C)
 const d = new D()
 assert.ne(d, c)
 assert.eq(d, new D())
 
+// Changing an instance property
 class E{
   constructor (i) { this.i = i }
   inc() { this.i++ }
@@ -95,17 +127,19 @@ assert.eq(e, new E(0))
 e.inc = function(){}
 assert.ne(e, new E(0))
 
+// Nested objects
 class DE{
   constructor (i) { this.e = new E(i) }
 }
 const de = new DE(0)
 assert.eq(de, new DE(0))
-de.x = 'x'
-assert.ne(de, new DE(0))
 assert.eq(new DE(c), new DE(c))
 assert.eq(new DE(de), new DE(de))
 assert.eq(new DE(m), new DE(m))
+de.x = 'x'
+assert.ne(de, new DE(0))
 
+// Circular references
 x.i = x
 y.i = x
 assert.eq(x, y)
@@ -116,6 +150,7 @@ x.i = y
 y.i = x
 assert.eq(x, y)
 
+// Subclasses of builtin types
 const abc = new String('abc')
 class STR extends String{
   constructor(s) {super(s)}
@@ -150,6 +185,12 @@ assert.eq(array, [1, 2])
 array.label = 'label'
 assert.ne(array, [1, 2])
 
+class Vector extends Array{
+  constructor(s) {super(s)}
+}
+assert.eq(new Vector(3), new Vector(3))
+assert.ne(new Vector(3), new Array(3))
+
 const set = new Set([1, 2])
 assert.eq(set, new Set([1, 2]))
 set.label = 'label'
@@ -160,7 +201,7 @@ assert.eq(map, new Map([[1, 2], [2,3]]))
 map.label = 'label'
 assert.ne(map, new Map([[1, 2], [2,3]]))
 
-// Assert with prototype inheritance
+// Prototype inheritance
 function A(n = 1) { this.n = n }
 A.prototype.inc = function(n) { this.n += n }
 const obja = new A(1)
@@ -169,7 +210,6 @@ assert.ne(obja, new A(2))
 
 function B(n = 1) { A.call(this, n) }
 assert.ne(obja, new B(1))
-// https://developer.mozilla.org/en-US/docs/Learn/JavaScript/Objects/Inheritance
 B.prototype = Object.create(A.prototype)
 assert.ne(obja, new B(1))
 B.prototype.dec = function(n) { this.inc(-n); }
@@ -181,57 +221,3 @@ assert.eq(objb, new B(1))
 assert.ne(objb, obja)
 objb.inc = () => {}
 assert.ne(objb, new B(1))
-
-
-// function STRB(s){
-//   String.call(this, s)
-// }
-// STRB.prototype = Object.create(String.prototype)
-// STRB.prototype.append = function(s) {this.a = s}
-// STRB.prototype.constructor = STRB
-// const strb = new STRB('abc')
-// const sw = new Set()
-// sw.add({i:1})
-// sw.add({i:1})
-// console.log([...sw])
-// console.log('------------------------')
-// console.log(isIterable(stringa))
-// console.log(stringa instanceof String)
-// console.log(stringa.constructor.name)
-// console.log(stringa.constructor === String)
-// console.log(typeof stringa)
-// console.log('------------------------')
-// console.log(isIterable(stringb))
-// console.log(stringb instanceof String)
-// console.log(stringb.constructor.name)
-// console.log(stringb.constructor === String)
-// console.log(typeof stringb)
-// console.log('------------------------')
-// console.log(isIterable(str))
-// console.log(str instanceof String)
-// console.log(str.constructor.name)
-// console.log(str.constructor === String)
-// console.log(typeof str)
-// console.log(str)
-// console.log('------------------------')
-// console.log(isIterable(strb))
-// console.log(strb instanceof String)
-// console.log(strb.constructor.name)
-// console.log(strb.constructor === STRB)
-// console.log(typeof strb)
-// console.log(strb)
-// console.log(getAllPropertyNames(s))
-// console.log(s.valueOf())
-// console.log(s.toString())
-// console.log(getAllPropertyNames(m))
-// console.log(getAllPropertyNames({}))
-// console.log('++++++++++++++++++++++++')
-// console.log(Object.getOwnPropertyNames(datea))
-// console.log(datea.prototype)
-// console.log(datea.constructor.prototype)
-// console.log(Object.getPrototypeOf(datea))
-// console.log('++++++++++++++++++++++++')
-// console.log(Object.getOwnPropertyNames(b))
-// console.log(b.prototype)
-// console.log(b.constructor.prototype)
-// console.log(Object.getPrototypeOf(b))
