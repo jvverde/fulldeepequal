dcabbabcfb5a980c2069144238b74732925e3c54
jvverde at win10 <jvverde@win10>
2020-11-17 21:53:39 +0000

    More tests

--- test/test4.spec.js
+++ test/test4.spec.js
@@ -4,19 +4,20 @@ const { random } = require('faker')
 describe('Strictly vs not strictly', () => {
   const n = random.number()
   const s = random.words(10)
-  describe('Different objects, but similar character by character', () => {
+  describe('Different objects, but similar toString() result', () => {
+    class A{}
+    class B{}
     const values = [
-      [{ f: function () {} }, { f: function () {} }]
+      [{ f: function () {} }, { f: function () {} }],
+      [new A(), new B()]
     ]
-    describe('A value must be equal to any other with same properties', () => {
-      for (const v of values) {
-        test(`${Object.entries(v[0])} should be (not strictly) equal to ${Object.entries(v[1])}`, () => {
-          const nok = isClone(v[0], v[1])
-          expect(nok).toBe(false)
-          const ok = isClone(v[0], v[1], { strictly: false })
-          expect(ok).toBe(true)
-        })
-      }
-    })
+    for (const v of values) {
+      test(`${Object.entries(v[0])} should be (not strictly) equal to ${Object.entries(v[1])}`, () => {
+        const nok = isClone(v[0], v[1])
+        expect(nok).toBe(false)
+        const ok = isClone(v[0], v[1], { strictly: false })
+        expect(ok).toBe(true)
+      })
+    }
   })
 })
